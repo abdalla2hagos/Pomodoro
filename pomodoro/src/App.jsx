498a6ea185ff7eab48423a9c50dbb783dfbc1e46
abdalla hagos <abdalla.hagos@gmail.com>
2023-06-14 20:14:28 -0400

    new features

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react'
+import Logo from './assets/logo'
+import IconSettings from './assets/IconSettings'
+import IconClose from './assets/IconClose'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>hi</h1>
+    <header>
+      <h1><Logo /></h1>
+
+      <nav>
+        <ul className="nav__menu">
+          <li>pomodoro</li>
+          <li>short break</li>
+          <li>long break</li>
+        </ul>
+      </nav>
+    </header>
+
+    <main className='main'>
+      <time>00:00</time>
+      <p>RESTART</p>
+    </main>
+
+    <footer>
+      <IconSettings />
+    </footer>
+
+    <dialog open>
+        <h2>Settings <span><IconClose /></span></h2>
+    </dialog>
     </>
   )
 }

19b30b64617727c76199ab7135326dbb7ff8dbf1
abdalla hagos <abdalla.hagos@gmail.com>
2023-06-15 20:11:39 -0400

    new features

--- pomodoro/src/App.jsx
+++ pomodoro/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import Logo from './assets/logo'
 import IconSettings from './assets/IconSettings'
 import IconClose from './assets/IconClose'
+import IconArrowUp from './assets/IconArrowUp'
+import IconArrowDown from './assets/IconArrowDown'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <header>
+    <header className='header'>
       <h1><Logo /></h1>
 
       <nav>
-        <ul className="nav__menu">
+        <ul className='nav__menu'>
           <li>pomodoro</li>
           <li>short break</li>
           <li>long break</li>
@@ -25,12 +28,82 @@ function App() {
       <p>RESTART</p>
     </main>
 
-    <footer>
+    <footer className='footer'>
       <IconSettings />
     </footer>
 
-    <dialog open>
-        <h2>Settings <span><IconClose /></span></h2>
+    <dialog>
+      <div className='flex--space'>
+        <h2 className='settings__title'>Settings</h2>
+        <IconClose />
+      </div>
+
+      <div>
+        <h3 className='time__title'>TIME (MINUTES)</h3>
+
+        <div>
+          <div className='flex--space'>
+            <p>pomodoro</p>
+
+            <div>
+              <p>25</p>
+
+              <div>
+                <IconArrowUp />
+                <IconArrowDown />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p>short break</p>
+
+            <div>
+              <p>5</p>
+
+              <div>
+                <IconArrowUp />
+                <IconArrowDown />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p>long break</p>
+
+            <div>
+              <p>15</p>
+
+              <div>
+                <IconArrowUp />
+                <IconArrowDown />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex--space'>
+        <h3>FONT</h3>
+
+        <div>
+          <button>Aa</button>
+          <button>Aa</button>
+          <button>Aa</button>
+        </div>
+      </div>
+
+      <div className='flex--space'>
+        <h3>COLOR</h3>
+
+        <div>
+          <button>orange</button>
+          <button>blue</button>
+          <button>purple</button>
+        </div>
+      </div>
+
+      <button>Apply</button>
     </dialog>
     </>
   )
